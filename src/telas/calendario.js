314e941b29eb67f509b017bdbb25eb4a673b98c4
André <alrakowski@inf.ufsm.br>
2020-05-14 14:36:54 -0300

    Header Tela Calendario

--- src/telas/calendario.js
+++ src/telas/calendario.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, ScrollView, Text, StyleSheet } from 'react-native'
 import { Calendar as Calendario } from 'react-native-plain-calendar'
+
+var year = new Date().getFullYear();
+var dayWeek = new Date().getDay();
+var month = new Date().getMonth();
+var day = new Date().getDate("dd");
+var days = ['Domingo','Segunda-feira','Terça-feira',
+            'Quarta-feira','Quinta-feira','Sexta-feira','Sábado'];
+var months = ['Janeiro','Fevereiro','Março',
+            'Abril','Maio','Junho','Julho',
+            'Agosto','Setembro','Outubro','Novembro','Dezembro'];
 
 export default class Calendar extends Component {
 
@@ -8,14 +18,58 @@ export default class Calendar extends Component {
         // Your code
     }
 
+    
     render() {
         return (
-            <View>
-                <Calendario.Picker 
-                    onSelected={this.onSelected}
-                    selectedType='single' 
-                />
-            </View>
+            <>
+                <View style={styles.header}>
+                    <Text style={styles.headerTextAno}>{year}</Text>
+                    <Text style={styles.headerTextDia}>{days[dayWeek]}, {day} de {months[month]}</Text>
+                </View>
+                <ScrollView style={styles.conteinerCalendar}>
+                    <Calendario.Picker style={styles.calendar}
+                        onSelected={this.onSelected}
+                        selectedType='single' 
+                        dayContainerStyle={styles.dayContainer}
+                        dayTextStyle={styles.dayText}
+                        daySingleSelectedStyle={styles.daySelectedView}
+                    />
+                </ScrollView>
+            </>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    headerTextAno:{
+        marginTop:30,
+        marginLeft:20,
+        fontSize:20,
+        color: "#D3D3D3", 
+    },
+    headerTextDia:{
+        marginTop:0,
+        marginLeft:20,
+        fontSize:30,
+        color: "#FFF", 
+    },
+    header:{
+        height:130,
+        backgroundColor:"#C58882"
+    },
+    calendar: {
+        marginTop:20
+    },
+    dayContainer: {
+        marginBottom:10,
+    },
+    dayText: {
+        fontSize:16,
+    },
+    conteinerCalendar: {
+       flex:1
+    },
+    daySelectedView:{
+        backgroundColor:"#C58882"
+    }
+}) 
